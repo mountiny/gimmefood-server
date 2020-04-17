54ed40eb8cf80a9fb6dc4b03520f4cba70d85905
Vit Horacek <2340623h@student.gla.ac.uk>
2020-04-17 19:32:21 +0100

    Build added

---
+++ build/precache-manifest.120eb8d5289e38a6fe0499a87039c5e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd4b9d3247ab1197fe6d92f2ebb43db0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "615b6303a5d1de28bd11",
+    "url": "/static/css/main.cb8cd96f.chunk.css"
+  },
+  {
+    "revision": "590a53492a109db9d8a2",
+    "url": "/static/js/2.29ea2d60.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.29ea2d60.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "615b6303a5d1de28bd11",
+    "url": "/static/js/main.8fb68b1d.chunk.js"
+  },
+  {
+    "revision": "67a6dd2ff48bc2fbbc72",
+    "url": "/static/js/runtime-main.c6978d5a.js"
+  }
+]);
