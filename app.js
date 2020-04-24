890527b807cb9cfd39b44f2e4f7e1c550248398f
Vit Horacek <2340623h@student.gla.ac.uk>
2020-04-24 06:31:51 +0100

    Server set up

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const loginRouter = require('./controllers/login')
 const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
+const helmet = require('helmet')
 
 logger.info('connecting to', config.MONGODB_URI)
 
@@ -23,11 +24,17 @@ mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology
 app.use(cors())
 app.use(express.static('build'))
 app.use(express.json())
+app.use(helmet)
 app.use(middleware.requestLogger)
 
 app.use('/api/users', usersRouter)
 app.use('/api/notes', notesRouter)
 app.use('/api/login', loginRouter)
+
+if (process.env.NODE_ENV === 'development') {
+  const testingRouter = require('./controllers/testing')
+  app.use('/api/testing', testingRouter)
+}
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
