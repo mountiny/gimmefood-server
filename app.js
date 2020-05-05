982da68354577b0c9a4a736dce84243eca5b75ec
Vit Horacek <2340623h@student.gla.ac.uk>
2020-05-05 08:45:35 +0100

    uibuild

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology
 
 app.use(cors())
 // app.use(express.static('build'))
-app.use(express.static(path.join(__dirname, 'client/build')))
+app.use(express.static(path.join(__dirname, '/build')))
 app.use(express.json())
 app.use(helmet())
 app.use(middleware.requestLogger)
@@ -53,7 +53,7 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 app.get('/*', (req, res) => {
-  let url = path.join(__dirname+'/client/build/index.html')
+  let url = path.join(__dirname+'/build/index.html')
   if (!url.startsWith('/shrouded-atoll-37097/')) // we're on local windows
     url = url.substring(1)
   res.sendFile(url)
